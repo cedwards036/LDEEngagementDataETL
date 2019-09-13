f2bbca5c36a034d5cbdc1dca80ac229b860cf0e7
Christopher Edwards <cedwards036@gmail.com>
2019-09-13 14:18:38 -0400

    Derive output file name from config

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from src.file_writer import write_engagement_data
 from src.office_hours import extract_office_hours_data, transform_office_hours_data
-from src.utils import BrowsingSession
+from src.utils import BrowsingSession, CONFIG
 
 if __name__ == '__main__':
     with BrowsingSession() as browser:
         raw_data = extract_office_hours_data(browser)
         clean_data = transform_office_hours_data(raw_data)
-        write_engagement_data('test.csv', clean_data)
+        write_engagement_data(CONFIG['engagement_data_filepath'], clean_data)
