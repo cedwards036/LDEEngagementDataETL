46a1b30f26f5eedc8824ee1cc5fbfdbfd32390f9
Christopher Edwards <cedwards036@gmail.com>
2019-10-11 09:44:29 -0400

    Fix accidental commenting-out of driver code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-from src.common import CONFIG
-from src.file_writers import write_to_csv, write_roster_excel_file
+from src.common import BrowsingSession, CONFIG
+from src.engagement_data_etl.career_fairs import run_career_fair_etl
+from src.engagement_data_etl.events import run_events_etl
+from src.engagement_data_etl.office_hours import run_office_hours_etl
+from src.file_writers import write_engagement_data, write_to_csv, write_roster_excel_file
 from src.student_data_etl.roster_data import run_data_file_etl, run_roster_file_etl
 
 # handshake data url: https://app.joinhandshake.com/analytics/explore_embed?insights_page=ZXhwbG9yZS9nZW5lcmF0ZWRfaGFuZHNoYWtlX3Byb2R1Y3Rpb24vc3R1ZGVudHM_cWlkPTdZa0VnSW5RQ1J2aGNBWE9PcXgyVkUmZW1iZWRfZG9tYWluPWh0dHBzOiUyRiUyRmFwcC5qb2luaGFuZHNoYWtlLmNvbSZ0b2dnbGU9Zmls
@@ -14,12 +17,12 @@ MAJORS_FILEPATH = f'{STUDENT_DATA_DIR}\\all_majors.csv'
 ATHLETE_FILEPATH = f'{STUDENT_DATA_DIR}\\student_athlete_roster_2019_09_19.csv'
 
 if __name__ == '__main__':
-    # with BrowsingSession() as browser:
-    # clean_appt_data = run_office_hours_etl(browser)
-    # clean_event_data = run_events_etl(browser)
-    # clean_fair_data = run_career_fair_etl(browser)
-    # engagement_data = clean_appt_data + clean_event_data + clean_fair_data
-    # write_engagement_data(CONFIG['engagement_data_filepath'], engagement_data)
+    with BrowsingSession() as browser:
+        clean_appt_data = run_office_hours_etl(browser)
+        clean_event_data = run_events_etl(browser)
+        clean_fair_data = run_career_fair_etl(browser)
+        engagement_data = clean_appt_data + clean_event_data + clean_fair_data
+        write_engagement_data(CONFIG['engagement_data_filepath'], engagement_data)
 
     student_data = run_data_file_etl(ROSTER_FILEPATHS, HANDSHAKE_DATA_FILEPATH, MAJORS_FILEPATH, ATHLETE_FILEPATH)
     roster_data = run_roster_file_etl(ROSTER_FILEPATHS, HANDSHAKE_DATA_FILEPATH, MAJORS_FILEPATH, ATHLETE_FILEPATH)
