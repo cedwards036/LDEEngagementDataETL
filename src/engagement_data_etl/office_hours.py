6d80362d21c797447f45cf3d711330359b95f3c6
Christopher Edwards <cedwards036@gmail.com>
2019-11-13 09:36:02 -0500

    Add explanatory suffix ("Office Hours" or "Appointment") to office hours engagement names

--- src/engagement_data_etl/office_hours.py
+++ src/engagement_data_etl/office_hours.py
@@ -35,18 +35,26 @@ def transform_office_hours_data(raw_data: List[dict]) -> List[EngagementRecord]:
 
 
 def _transform_data_row(raw_data_row: dict) -> EngagementRecord:
+    department = _get_department_from_type(raw_data_row)
     return EngagementRecord(
         engagement_type=EngagementTypes.OFFICE_HOURS,
         handshake_engagement_id=raw_data_row[AppointmentFields.ID],
         start_date_time=parse_date_string(raw_data_row[AppointmentFields.START_DATE_TIME]),
         medium=_get_medium(raw_data_row),
-        engagement_name=raw_data_row[AppointmentFields.TYPE],
-        engagement_department=_get_department_from_type(raw_data_row),
+        engagement_name=_make_engagement_name(raw_data_row, department),
+        engagement_department=department,
         student_handshake_id=raw_data_row[AppointmentFields.STUDENT_ID],
         student_school_year_at_time_of_engagement=raw_data_row[AppointmentFields.STUDENT_SCHOOL_YEAR],
         student_pre_registered=_student_pre_registered(raw_data_row),
         associated_staff_email=raw_data_row[AppointmentFields.STAFF_MEMBER_EMAIL]
     )
+
+
+def _make_engagement_name(raw_data_row: dict, department: Department) -> str:
+    if department == Departments.PRE_PROF.value:
+        return f'{raw_data_row[AppointmentFields.TYPE]} Appointment'
+    else:
+        return f'{raw_data_row[AppointmentFields.TYPE]} Office Hours'
 
 
 def _get_medium(raw_data_row: dict) -> Mediums:
@@ -63,37 +71,38 @@ def _get_medium(raw_data_row: dict) -> Mediums:
 
 def _get_department_from_type(raw_data_row: dict) -> Department:
     appt_type_to_dept_mapping = {
-        'Homewood: AMS Career Advising - For FM, AMS, and Data Science Graduate Students': Departments.AMS_FM_DATA_SCI.value,
-        'Homewood: Biological and Brain Sciences': Departments.BIO_BRAIN_SCI.value,
-        'Homewood: Biomedical Engineering': Departments.BME.value,
-        'Homewood: ChemBE and Materials Science Engineering': Departments.CHEMBE_MAT_SCI.value,
-        'Homewood: Computer Science, Computer Engineering, and Electrical Engineering': Departments.COMP_ELEC_ENG.value,
-        'Homewood: Engineering Masters Students': Departments.ENG_MASTERS.value,
-        'Homewood: Humanities: History, Philosophy, and Humanistic Thought': Departments.HIST_PHIL_HUM.value,
-        'Homewood: Humanities: Language, Literatures, Film and Media': Departments.LIT_LANG_FILM.value,
-        'Homewood: Social Sciences: Political Science, Economics, and Finance': Departments.POL_ECON_FIN.value,
-        'Homewood: Misc. Engineering': Departments.MISC_ENG.value,
-        'Homewood: Peer Advisor Drop In': Departments.PA_DROP_INS.value,
-        'Homewood: Physical and Environmental Sciences': Departments.PHYS_ENV_SCI.value,
-        'Homewood: Pre-Health and Public Health Studies': Departments.PRE_PUB_HEALTH.value,
-        'Homewood: SOAR: Athletics': Departments.SOAR_ATHLETICS.value,
-        'Homewood: SOAR: First Year Experience (KSAS)': Departments.SOAR_FYE_KSAS.value,
-        'Homewood: SOAR: First Year Experience (WSE)': Departments.SOAR_FYE_WSE.value,
-        'Homewood: SOAR: Student Leadership and Involvement': Departments.SOAR_SLI.value,
-        'Homewood: Social Sciences: International Studies, Sociology, and Anthropology': Departments.INT_SOC_ANTH.value,
-        'Homewood: Arts, Media, and Marketing Academy': Departments.AMM_ACADEMY.value,
-        'Homewood: Nonprofit and Government Academy': Departments.NP_GOV_ACADEMY.value,
-        'Homewood: Consulting Academy': Departments.CONSUTING_ACADEMY.value,
-        'Homewood: Finance Academy': Departments.FINANCE_ACADEMY.value,
-        'Homewood: Health Sciences Academy': Departments.HEALTH_SCI_ACADEMY.value,
-        'Homewood: STEM and Innovation Academy': Departments.STEM_ACADEMY.value,
-        'Homewood: Pre-Law': Departments.PRE_PROF.value,
-        'Homewood: Pre-Health/Other Health Professions': Departments.PRE_PROF.value,
-        'Homewood: Pre-Dental': Departments.PRE_PROF.value,
-        'Homewood: Pre-Med': Departments.PRE_PROF.value,
-        'Homewood: Non-Office Hour Interaction': Departments.NO_DEPARTMENT.value
+        'Homewood: AMS Career Advising - For FM, AMS, and Data Science Graduate Students': Departments.AMS_FM_DATA_SCI,
+        'Homewood: Biological and Brain Sciences': Departments.BIO_BRAIN_SCI,
+        'Homewood: Biomedical Engineering': Departments.BME,
+        'Homewood: ChemBE and Materials Science Engineering': Departments.CHEMBE_MAT_SCI,
+        'Homewood: Computer Science, Computer Engineering, and Electrical Engineering': Departments.COMP_ELEC_ENG,
+        'Homewood: Engineering Masters Students': Departments.ENG_MASTERS,
+        'Homewood: Humanities: History, Philosophy, and Humanistic Thought': Departments.HIST_PHIL_HUM,
+        'Homewood: Humanities: Language, Literatures, Film and Media': Departments.LIT_LANG_FILM,
+        'Homewood: Social Sciences: Political Science, Economics, and Finance': Departments.POL_ECON_FIN,
+        'Homewood: Misc. Engineering': Departments.MISC_ENG,
+        'Homewood: Peer Advisor Drop In': Departments.PA_DROP_INS,
+        'Homewood: Physical and Environmental Sciences': Departments.PHYS_ENV_SCI,
+        'Homewood: Pre-Health and Public Health Studies': Departments.PRE_PUB_HEALTH,
+        'Homewood: SOAR: Athletics': Departments.SOAR_ATHLETICS,
+        'Homewood: SOAR: First Year Experience (KSAS)': Departments.SOAR_FYE_KSAS,
+        'Homewood: SOAR: First Year Experience (WSE)': Departments.SOAR_FYE_WSE,
+        'Homewood: SOAR: Student Leadership and Involvement': Departments.SOAR_SLI,
+        'Homewood: Social Sciences: International Studies, Sociology, and Anthropology': Departments.INT_SOC_ANTH,
+        'Homewood: Arts, Media, and Marketing Academy': Departments.AMM_ACADEMY,
+        'Homewood: Nonprofit and Government Academy': Departments.NP_GOV_ACADEMY,
+        'Homewood: Consulting Academy': Departments.CONSUTING_ACADEMY,
+        'Homewood: Finance Academy': Departments.FINANCE_ACADEMY,
+        'Homewood: Health Sciences Academy': Departments.HEALTH_SCI_ACADEMY,
+        'Homewood: STEM and Innovation Academy': Departments.STEM_ACADEMY,
+        'Homewood: Pre-Law': Departments.PRE_PROF,
+        'Homewood: Pre-Health/Other Health Professions': Departments.PRE_PROF,
+        'Homewood: Pre-Health': Departments.PRE_PROF,
+        'Homewood: Pre-Dental': Departments.PRE_PROF,
+        'Homewood: Pre-Med': Departments.PRE_PROF,
+        'Homewood: Non-Office Hour Interaction': Departments.NO_DEPARTMENT
     }
-    return appt_type_to_dept_mapping[raw_data_row[AppointmentFields.TYPE]]
+    return appt_type_to_dept_mapping[raw_data_row[AppointmentFields.TYPE]].value
 
 
 def _student_pre_registered(raw_data_row: dict) -> bool:
