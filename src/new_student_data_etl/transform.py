c90d843fcc2e2c6b74777dcc319688814ec67bd9
Christopher Edwards <cedwards036@gmail.com>
2020-04-16 14:43:24 -0400

    add methods to create a student department mapping table

---
+++ src/new_student_data_etl/transform.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from src.data_model import Departments
+
+def add_major_metadata(students: pd.DataFrame, major_metadata: pd.DataFrame) -> pd.DataFrame:
+    result = students.merge(major_metadata, how='left', on='major')
+    result = result.rename(columns={'department': 'major_department'})
+    return result
+
+def make_student_department_table(students: pd.DataFrame) -> pd.DataFrame:
+    if students.empty:
+        return pd.DataFrame(data={'hopkins_id': [], 'department': []})
+    else:
+        sub_tables = []
+        for hopkins_id in students['hopkins_id'].unique():
+            sub_tables.append(make_student_department_subtable(students, hopkins_id))
+        return pd.concat(sub_tables, ignore_index=True)
+
+def make_student_department_subtable(students: pd.DataFrame, hopkins_id: str) -> pd.DataFrame:
+
+    def create_student_dept_table_from_major_depts(df: pd.DataFrame) -> pd.DataFrame:
+        student_dept_table = df[['hopkins_id', 'major_department']]
+        return student_dept_table.rename(columns={'major_department': 'department'})
+
+    def add_soar_departments(student_df: pd.DataFrame, table_df: pd.DataFrame) -> pd.DataFrame:
+        if student_df.iloc[0]['school_year'] == 'Freshman':
+            if 'ksas' in student_df['college'].values:
+                table_df = append_department(Departments.SOAR_FYE_KSAS.value.name, table_df)
+            if 'wse' in student_df['college'].values:
+                table_df = append_department(Departments.SOAR_FYE_WSE.value.name, table_df)
+        if student_df.iloc[0]['is_athlete']:
+            table_df = append_department(Departments.SOAR_ATHLETICS.value.name, table_df)
+        if student_df.iloc[0]['is_urm']:
+            table_df = append_department(Departments.SOAR_DIV_INCL.value.name, table_df)
+        return table_df
+
+    def append_department(department: str, table: pd.DataFrame) -> pd.DataFrame:
+        return table.append(pd.DataFrame(data={
+            'hopkins_id': [hopkins_id],
+            'department': [department]
+        }), ignore_index=True)
+
+    student_rows = students.loc[students['hopkins_id'] == hopkins_id]
+    student_dept_table = create_student_dept_table_from_major_depts(student_rows)
+    if not student_rows.empty:
+        student_dept_table = add_soar_departments(student_rows, student_dept_table)
+    student_dept_table = student_dept_table.drop_duplicates().reset_index(drop=True)
+    return student_dept_table
---
+++ test/unit_tests/test_new_student_data_etl/test_transform.py
@@ -0,0 +1,198 @@
+import unittest
+
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from src.new_student_data_etl.transform import add_major_metadata
+from src.new_student_data_etl.transform import make_student_department_subtable
+from src.new_student_data_etl.transform import make_student_department_table
+from src.data_model import Departments
+
+
+class TestAddStudentColleges(unittest.TestCase):
+
+    def test_left_joins_student_data_to_major_metadata_using_major_as_the_join_key(self):
+        students = pd.DataFrame(data={'major': ['economics', 'comp sci', 'economics']})
+        major_metadata = pd.DataFrame(data={'major': ['economics', 'comp sci'], 'college': ['ksas', 'wse'], 'department': ['pol_sci_econ', 'comp_elec_eng']})
+        expected = pd.DataFrame(data={
+            'major': ['economics', 'comp sci', 'economics'],
+            'college': ['ksas', 'wse', 'ksas'],
+            'major_department': ['pol_sci_econ', 'comp_elec_eng', 'pol_sci_econ']
+        })
+        assert_frame_equal(expected, add_major_metadata(students, major_metadata))
+
+
+class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
+
+    def test_makes_an_empty_dataframe_if_the_given_hopkins_id_does_not_appear_in_the_students_table(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': [],
+            'college': [],
+            'school_year': [],
+            'is_athlete': [],
+            'is_urm': [],
+            'major_department': []
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': [],
+            'department': []
+        })
+        assert_frame_equal(expected, make_student_department_subtable(students, 'invalid id'))
+
+    def test_makes_a_dataframe_mapping_hopkins_id_to_academic_departments_if_student_doesnt_fall_into_any_soar_departments(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': ['jf38ru', 'jf38ru', 'kf034i'],
+            'college': ['ksas', 'wse', 'ksas'],
+            'school_year': ['Senior', 'Senior', 'Junior'],
+            'is_athlete': [False, False, False],
+            'is_urm': [False, False, False],
+            'major_department': ['pol_sci_econ', 'comp_elec_eng', 'pol_sci_econ']
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': ['jf38ru', 'jf38ru'],
+            'department': ['pol_sci_econ', 'comp_elec_eng']
+        })
+        assert_frame_equal(expected, make_student_department_subtable(students, 'jf38ru'))
+
+
+    def test_includes_soar_fye_ksas_department_for_ksas_freshmen_who_have_already_chosen_a_major(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': ['93aml3'],
+            'college': ['ksas'],
+            'school_year': ['Freshman'],
+            'is_athlete': [False],
+            'is_urm': [False],
+            'major_department': ['pol_sci_econ']
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': ['93aml3', '93aml3'],
+            'department': ['pol_sci_econ', Departments.SOAR_FYE_KSAS.value.name]
+        })
+        assert_frame_equal(expected, make_student_department_subtable(students, '93aml3'))
+
+    def test_only_includes_soar_fye_ksas_department_once_for_ksas_freshmen_who_have_not_chosen_a_major(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': ['93aml3'],
+            'college': ['ksas'],
+            'school_year': ['Freshman'],
+            'is_athlete': [False],
+            'is_urm': [False],
+            'major_department': [Departments.SOAR_FYE_KSAS.value.name]
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': ['93aml3'],
+            'department': [Departments.SOAR_FYE_KSAS.value.name]
+        })
+        assert_frame_equal(expected, make_student_department_subtable(students, '93aml3'))
+
+    def test_includes_soar_fye_wse_department_for_wse_freshmen_who_have_already_chosen_a_major(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': ['93aml3'],
+            'college': ['wse'],
+            'school_year': ['Freshman'],
+            'is_athlete': [False],
+            'is_urm': [False],
+            'major_department': ['comp_sci']
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': ['93aml3', '93aml3'],
+            'department': ['comp_sci', Departments.SOAR_FYE_WSE.value.name]
+        })
+        assert_frame_equal(expected, make_student_department_subtable(students, '93aml3'))
+
+    def test_includes_soar_athletics_department_for_athletes(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': ['jf398f'],
+            'college': ['wse'],
+            'school_year': ['Sophomore'],
+            'is_athlete': [True],
+            'is_urm': [False],
+            'major_department': ['comp_sci']
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': ['jf398f', 'jf398f'],
+            'department': ['comp_sci', Departments.SOAR_ATHLETICS.value.name]
+        })
+        assert_frame_equal(expected, make_student_department_subtable(students, 'jf398f'))
+
+    def test_includes_soar_diversity_and_inclusion_department_for_urms(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': ['pmle45'],
+            'college': ['wse'],
+            'school_year': ['Sophomore'],
+            'is_athlete': [False],
+            'is_urm': [True],
+            'major_department': ['elec_eng']
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': ['pmle45', 'pmle45'],
+            'department': ['elec_eng', Departments.SOAR_DIV_INCL.value.name]
+        })
+        assert_frame_equal(expected, make_student_department_subtable(students, 'pmle45'))
+
+    def test_includes_multiple_soar_departments_as_appropriate(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': ['pmle45', 'pmle45'],
+            'college': ['wse', 'ksas'],
+            'school_year': ['Freshman', 'Freshman'],
+            'is_athlete': [True, True],
+            'is_urm': [True, True],
+            'major_department': ['elec_eng', Departments.SOAR_FYE_KSAS.value.name]
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': ['pmle45', 'pmle45', 'pmle45', 'pmle45', 'pmle45'],
+            'department': [
+                'elec_eng',
+                Departments.SOAR_FYE_KSAS.value.name,
+                Departments.SOAR_FYE_WSE.value.name,
+                Departments.SOAR_ATHLETICS.value.name,
+                Departments.SOAR_DIV_INCL.value.name]
+        })
+        assert_frame_equal(expected, make_student_department_subtable(students, 'pmle45'))
+
+class TestMakeStudentDepartmentsTable(unittest.TestCase):
+
+    def test_makes_an_empty_dataframe_if_given_an_empty_student_table(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': [],
+            'college': [],
+            'school_year': [],
+            'is_athlete': [],
+            'is_urm': [],
+            'major_department': []
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': [],
+            'department': []
+        })
+        assert_frame_equal(expected, make_student_department_table(students))
+
+    def test_does_the_same_thing_as_subtable_if_there_is_only_one_student(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': ['93aml3'],
+            'college': ['ksas'],
+            'school_year': ['Freshman'],
+            'is_athlete': [False],
+            'is_urm': [False],
+            'major_department': ['pol_sci_econ']
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': ['93aml3', '93aml3'],
+            'department': ['pol_sci_econ', Departments.SOAR_FYE_KSAS.value.name]
+        })
+        assert_frame_equal(expected, make_student_department_table(students))
+
+    def test_makes_a_table_mapping_every_student_to_their_academic_and_soar_departments(self):
+        students = pd.DataFrame(data={
+            'hopkins_id': ['93aml3', 'ndfe83', '78576e'],
+            'college': ['ksas', 'ksas', 'wse'],
+            'school_year': ['Sophomore', 'Sophomore', 'Junior'],
+            'is_athlete': [False, False, False],
+            'is_urm': [False, False, False],
+            'major_department': ['pol_sci_econ', 'brain_sci', 'comp_sci']
+        })
+        expected = pd.DataFrame(data={
+            'hopkins_id': ['93aml3', 'ndfe83', '78576e'],
+            'department': ['pol_sci_econ', 'brain_sci', 'comp_sci']
+        })
+        assert_frame_equal(expected, make_student_department_table(students))
