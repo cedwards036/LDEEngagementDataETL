b818613ebc9b9a9cb6fdbc0d21662a19eb85b3f8
Christopher Edwards <cedwards036@gmail.com>
2020-04-17 12:01:18 -0400

    Add method to merge handshake data with student sis data

--- src/new_student_data_etl/transform_student_data.py
+++ src/new_student_data_etl/transform_student_data.py
@@ -1,5 +1,11 @@
 import pandas as pd
+
 from src.data_model import Departments
+
+
+def merge_with_handshake_data(students: pd.DataFrame, handshake_data: pd.DataFrame) -> pd.DataFrame:
+    return students.merge(handshake_data, how='left', on='jhed')
+
 
 def melt_majors(students: pd.DataFrame) -> pd.DataFrame:
     split_majors = students['majors'].str.split(';', expand=True)
@@ -11,10 +17,12 @@ def melt_majors(students: pd.DataFrame) -> pd.DataFrame:
     students = students.drop(columns=['majors'])
     return students
 
+
 def add_major_metadata(students: pd.DataFrame, major_metadata: pd.DataFrame) -> pd.DataFrame:
     result = students.merge(major_metadata, how='left', on='major')
     result = result.rename(columns={'department': 'major_department'})
     return result
+
 
 def make_student_department_table(students: pd.DataFrame) -> pd.DataFrame:
     if students.empty:
@@ -25,8 +33,8 @@ def make_student_department_table(students: pd.DataFrame) -> pd.DataFrame:
             sub_tables.append(make_student_department_subtable(students, hopkins_id))
         return pd.concat(sub_tables, ignore_index=True)
 
-def make_student_department_subtable(students: pd.DataFrame, hopkins_id: str) -> pd.DataFrame:
 
+def make_student_department_subtable(students: pd.DataFrame, hopkins_id: str) -> pd.DataFrame:
     def create_student_dept_table_from_major_depts(df: pd.DataFrame) -> pd.DataFrame:
         student_dept_table = df[['hopkins_id', 'major_department']]
         return student_dept_table.rename(columns={'major_department': 'department'})
--- test/unit_tests/test_new_student_data_etl/test_transform_student_data.py
+++ test/unit_tests/test_new_student_data_etl/test_transform_student_data.py
@@ -3,19 +3,20 @@ import unittest
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
+from src.data_model import Departments
 from src.new_student_data_etl.transform_student_data import add_major_metadata
 from src.new_student_data_etl.transform_student_data import make_student_department_subtable
 from src.new_student_data_etl.transform_student_data import make_student_department_table
 from src.new_student_data_etl.transform_student_data import melt_majors
-from src.data_model import Departments
+from src.new_student_data_etl.transform_student_data import merge_with_handshake_data
 
 
 class TestAddStudentColleges(unittest.TestCase):
 
     def test_left_joins_student_data_to_major_metadata_using_major_as_the_join_key(self):
-        students = pd.DataFrame(data={'major': ['economics', 'comp sci', 'economics']})
-        major_metadata = pd.DataFrame(data={'major': ['economics', 'comp sci'], 'college': ['ksas', 'wse'], 'department': ['pol_sci_econ', 'comp_elec_eng']})
-        expected = pd.DataFrame(data={
+        students = pd.DataFrame({'major': ['economics', 'comp sci', 'economics']})
+        major_metadata = pd.DataFrame({'major': ['economics', 'comp sci'], 'college': ['ksas', 'wse'], 'department': ['pol_sci_econ', 'comp_elec_eng']})
+        expected = pd.DataFrame({
             'major': ['economics', 'comp sci', 'economics'],
             'college': ['ksas', 'wse', 'ksas'],
             'major_department': ['pol_sci_econ', 'comp_elec_eng', 'pol_sci_econ']
@@ -26,25 +27,37 @@ class TestAddStudentColleges(unittest.TestCase):
 class TestMeltMajors(unittest.TestCase):
 
     def test_leaves_row_with_only_one_major_unchanged_except_for_the_column_name(self):
-        students = pd.DataFrame(data={'hopkins_id': ['fi9485'], 'majors': ['economics']})
-        expected = pd.DataFrame(data={'hopkins_id': ['fi9485'], 'major': ['economics']})
+        students = pd.DataFrame({'hopkins_id': ['fi9485'], 'majors': ['economics']})
+        expected = pd.DataFrame({'hopkins_id': ['fi9485'], 'major': ['economics']})
         assert_frame_equal(expected, melt_majors(students))
 
     def test_splits_major_string_into_separate_columns_and_unpivots_those_columns_into_a_single_major_column(self):
-        students = pd.DataFrame(data={'hopkins_id': ['uu9d32'], 'majors': ['economics;math;science']})
-        expected = pd.DataFrame(data={'hopkins_id': ['uu9d32', 'uu9d32', 'uu9d32'], 'major': ['economics', 'math', 'science']})
+        students = pd.DataFrame({'hopkins_id': ['uu9d32'], 'majors': ['economics;math;science']})
+        expected = pd.DataFrame({'hopkins_id': ['uu9d32', 'uu9d32', 'uu9d32'], 'major': ['economics', 'math', 'science']})
         assert_frame_equal(expected, melt_majors(students))
 
     def test_only_includes_rows_for_non_null_majors(self):
-        students = pd.DataFrame(data={'hopkins_id': ['uu9d32', '938tjg'], 'majors': ['economics;math;science', 'english']})
-        expected = pd.DataFrame(data={'hopkins_id': ['uu9d32', 'uu9d32', 'uu9d32', '938tjg'], 'major': ['economics', 'math', 'science', 'english']})
+        students = pd.DataFrame({'hopkins_id': ['uu9d32', '938tjg'], 'majors': ['economics;math;science', 'english']})
+        expected = pd.DataFrame({'hopkins_id': ['uu9d32', 'uu9d32', 'uu9d32', '938tjg'], 'major': ['economics', 'math', 'science', 'english']})
         assert_frame_equal(expected, melt_majors(students))
+
+
+class TestMergeWithHandshakeData(unittest.TestCase):
+
+    def test_merges_student_data_with_handshake_data_on_jhed(self):
+        students = pd.DataFrame({'jhed': ['asmi34', 'asmi34', 'bmart15']})
+        handshake_data = pd.DataFrame({'jhed': ['asmi34', 'bmart15'], 'handshake_field': ['value1', 'value2']})
+        expected = pd.DataFrame({
+            'jhed': ['asmi34', 'asmi34', 'bmart15'],
+            'handshake_field': ['value1', 'value1', 'value2']
+        })
+        assert_frame_equal(expected, merge_with_handshake_data(students, handshake_data))
 
 
 class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
 
     def test_makes_an_empty_dataframe_if_the_given_hopkins_id_does_not_appear_in_the_students_table(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': [],
             'college': [],
             'school_year': [],
@@ -52,14 +65,14 @@ class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
             'is_urm': [],
             'major_department': []
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': [],
             'department': []
         })
         assert_frame_equal(expected, make_student_department_subtable(students, 'invalid id'))
 
     def test_makes_a_dataframe_mapping_hopkins_id_to_academic_departments_if_student_doesnt_fall_into_any_soar_departments(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': ['jf38ru', 'jf38ru', 'kf034i'],
             'college': ['ksas', 'wse', 'ksas'],
             'school_year': ['Senior', 'Senior', 'Junior'],
@@ -67,15 +80,14 @@ class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
             'is_urm': [False, False, False],
             'major_department': ['pol_sci_econ', 'comp_elec_eng', 'pol_sci_econ']
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': ['jf38ru', 'jf38ru'],
             'department': ['pol_sci_econ', 'comp_elec_eng']
         })
         assert_frame_equal(expected, make_student_department_subtable(students, 'jf38ru'))
 
-
     def test_includes_soar_fye_ksas_department_for_ksas_freshmen_who_have_already_chosen_a_major(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': ['93aml3'],
             'college': ['ksas'],
             'school_year': ['Freshman'],
@@ -83,14 +95,14 @@ class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
             'is_urm': [False],
             'major_department': ['pol_sci_econ']
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': ['93aml3', '93aml3'],
             'department': ['pol_sci_econ', Departments.SOAR_FYE_KSAS.value.name]
         })
         assert_frame_equal(expected, make_student_department_subtable(students, '93aml3'))
 
     def test_only_includes_soar_fye_ksas_department_once_for_ksas_freshmen_who_have_not_chosen_a_major(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': ['93aml3'],
             'college': ['ksas'],
             'school_year': ['Freshman'],
@@ -98,14 +110,14 @@ class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
             'is_urm': [False],
             'major_department': [Departments.SOAR_FYE_KSAS.value.name]
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': ['93aml3'],
             'department': [Departments.SOAR_FYE_KSAS.value.name]
         })
         assert_frame_equal(expected, make_student_department_subtable(students, '93aml3'))
 
     def test_includes_soar_fye_wse_department_for_wse_freshmen_who_have_already_chosen_a_major(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': ['93aml3'],
             'college': ['wse'],
             'school_year': ['Freshman'],
@@ -113,14 +125,14 @@ class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
             'is_urm': [False],
             'major_department': ['comp_sci']
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': ['93aml3', '93aml3'],
             'department': ['comp_sci', Departments.SOAR_FYE_WSE.value.name]
         })
         assert_frame_equal(expected, make_student_department_subtable(students, '93aml3'))
 
     def test_includes_soar_athletics_department_for_athletes(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': ['jf398f'],
             'college': ['wse'],
             'school_year': ['Sophomore'],
@@ -128,14 +140,14 @@ class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
             'is_urm': [False],
             'major_department': ['comp_sci']
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': ['jf398f', 'jf398f'],
             'department': ['comp_sci', Departments.SOAR_ATHLETICS.value.name]
         })
         assert_frame_equal(expected, make_student_department_subtable(students, 'jf398f'))
 
     def test_includes_soar_diversity_and_inclusion_department_for_urms(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': ['pmle45'],
             'college': ['wse'],
             'school_year': ['Sophomore'],
@@ -143,14 +155,14 @@ class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
             'is_urm': [True],
             'major_department': ['elec_eng']
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': ['pmle45', 'pmle45'],
             'department': ['elec_eng', Departments.SOAR_DIV_INCL.value.name]
         })
         assert_frame_equal(expected, make_student_department_subtable(students, 'pmle45'))
 
     def test_includes_multiple_soar_departments_as_appropriate(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': ['pmle45', 'pmle45'],
             'college': ['wse', 'ksas'],
             'school_year': ['Freshman', 'Freshman'],
@@ -158,7 +170,7 @@ class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
             'is_urm': [True, True],
             'major_department': ['elec_eng', Departments.SOAR_FYE_KSAS.value.name]
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': ['pmle45', 'pmle45', 'pmle45', 'pmle45', 'pmle45'],
             'department': [
                 'elec_eng',
@@ -169,10 +181,11 @@ class TestMakeStudentDepartmentsSubTable(unittest.TestCase):
         })
         assert_frame_equal(expected, make_student_department_subtable(students, 'pmle45'))
 
+
 class TestMakeStudentDepartmentsTable(unittest.TestCase):
 
     def test_makes_an_empty_dataframe_if_given_an_empty_student_table(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': [],
             'college': [],
             'school_year': [],
@@ -180,14 +193,14 @@ class TestMakeStudentDepartmentsTable(unittest.TestCase):
             'is_urm': [],
             'major_department': []
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': [],
             'department': []
         })
         assert_frame_equal(expected, make_student_department_table(students))
 
     def test_does_the_same_thing_as_subtable_if_there_is_only_one_student(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': ['93aml3'],
             'college': ['ksas'],
             'school_year': ['Freshman'],
@@ -195,14 +208,14 @@ class TestMakeStudentDepartmentsTable(unittest.TestCase):
             'is_urm': [False],
             'major_department': ['pol_sci_econ']
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': ['93aml3', '93aml3'],
             'department': ['pol_sci_econ', Departments.SOAR_FYE_KSAS.value.name]
         })
         assert_frame_equal(expected, make_student_department_table(students))
 
     def test_makes_a_table_mapping_every_student_to_their_academic_and_soar_departments(self):
-        students = pd.DataFrame(data={
+        students = pd.DataFrame({
             'hopkins_id': ['93aml3', 'ndfe83', '78576e'],
             'college': ['ksas', 'ksas', 'wse'],
             'school_year': ['Sophomore', 'Sophomore', 'Junior'],
@@ -210,7 +223,7 @@ class TestMakeStudentDepartmentsTable(unittest.TestCase):
             'is_urm': [False, False, False],
             'major_department': ['pol_sci_econ', 'brain_sci', 'comp_sci']
         })
-        expected = pd.DataFrame(data={
+        expected = pd.DataFrame({
             'hopkins_id': ['93aml3', 'ndfe83', '78576e'],
             'department': ['pol_sci_econ', 'brain_sci', 'comp_sci']
         })
