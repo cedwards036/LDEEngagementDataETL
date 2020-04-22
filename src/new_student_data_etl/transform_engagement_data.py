67360a135554fe0d1885c4719383ad9819b76cd0
Christopher Edwards <cedwards036@gmail.com>
2020-04-22 15:13:20 -0400

    Add function for summarizing engagement data for each student

---
+++ src/new_student_data_etl/transform_engagement_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+def count_engagements_by_type(engagements: pd.DataFrame) -> pd.DataFrame:
+
+    def groupby_handshake_id_and_engagement_type(df: pd.DataFrame) -> pd.DataFrame:
+        grouped_df = df.groupby(['student_handshake_id', 'engagement_type'])['unique_engagement_id'].count().reset_index()
+        grouped_df['engagement_type'] = grouped_df['engagement_type'] + '_engagements'
+        return grouped_df
+
+    def pivot_engagement_types(df):
+        pivoted_df = df.pivot(index='student_handshake_id', columns='engagement_type', values='unique_engagement_id').fillna(0).reset_index()
+        pivoted_df.axes[1].name = None
+        pivoted_df.iloc[:, 1:] = pivoted_df.iloc[:, 1:].astype(np.int64)
+        return pivoted_df
+
+    grouped_engagements = groupby_handshake_id_and_engagement_type(engagements)
+    return pivot_engagement_types(grouped_engagements)
---
+++ test/unit_tests/test_new_student_data_etl/test_transform_engagement_data.py
@@ -0,0 +1,26 @@
+import unittest
+
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from src.new_student_data_etl.transform_engagement_data import count_engagements_by_type
+
+
+class TestCountEngagementsByType(unittest.TestCase):
+
+    def test_groups_engagement_data_by_handshake_id_and_counts_engagements_by_type(self):
+        engagements = pd.DataFrame({
+            'student_handshake_id': ['8029382', '8029382', '8029382', '8029382', '4738743'],
+            'engagement_type': ['office_hours', 'event', 'event', 'interview', 'vmock'],
+            'unique_engagement_id': ['1a4f32r', '028j9g4h3', 'f09j09g43', '0f902fj83', 'dd8dj9g8g'],
+            'extra_field': ['', '', '', '', '']
+        })
+        expected = pd.DataFrame({
+            'student_handshake_id': ['4738743', '8029382'],
+            'event_engagements': [0, 2],
+            'interview_engagements': [0, 1],
+            'office_hours_engagements': [0, 1],
+            'vmock_engagements': [1, 0]
+        })
+
+        assert_frame_equal(expected, count_engagements_by_type(engagements))
