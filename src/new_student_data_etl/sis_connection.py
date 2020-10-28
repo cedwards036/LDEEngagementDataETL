92a6716bfa3ddcaa41c0ea0a8c553e7d6b2d3113
Christopher Edwards <cedwards036@gmail.com>
2020-10-28 13:49:23 -0400

    remove login step from SIS connection
    Logging in is unnecessary when running the script from a computer while already logged into the Hopkins VPN

--- src/new_student_data_etl/sis_connection.py
+++ src/new_student_data_etl/sis_connection.py
@@ -1,29 +1,28 @@
 import os
-import pyodbc
 from typing import List
 
-from getpass import getpass
+import pyodbc
 
 from src.common import load_config
 
 CONFIG_FILEPATH = f'{os.path.dirname(os.path.abspath(__file__))}/../../sis_config.json'
 SIS_CONFIG = load_config(CONFIG_FILEPATH)
 
+
 class SISConnection:
 
     def __enter__(self):
-        password = getpass('Please enter your SIS password: ')
         self.cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + \
-                              SIS_CONFIG['server'] + ';DATABASE=' + \
-                              SIS_CONFIG['database'] + ';UID=' + \
-                              SIS_CONFIG['username'] + ';PWD=' + password + \
-                              ';Trusted_Connection=yes')
+                                   SIS_CONFIG['server'] + ';DATABASE=' + \
+                                   SIS_CONFIG['database'] + ';UID=' +
+                                   ';Trusted_Connection=yes')
         self.cursor = self.cnxn.cursor()
         return SISCursor(self.cursor)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.cursor.close()
         self.cnxn.close()
+
 
 class SISCursor:
 
